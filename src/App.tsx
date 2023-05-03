40086f920eaf5f2bb217062bdcad6e6acf601137
YousafAhmed <yousafahmed537@gmail.com>
2023-05-03 16:41:59 +0500

    modified:   src/App.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   src/Home.tsx 	modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Header } from "antd/lib/layout/layout";
 import { Home } from "./Home";
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header>
+        <Home />
+      </Header>
     </div>
   );
 }
