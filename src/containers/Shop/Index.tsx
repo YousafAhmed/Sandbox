e9dd626d6f95e1e0c75aa08b0188433b0d1a678c
YousafAhmed <yousafahmed537@gmail.com>
2023-05-25 12:00:59 +0500

    modified:   src/App.tsx 	modified:   src/ReduxStore/RootSaga/rootSaga.ts 	modified:   src/ReduxStore/Store/store.ts 	modified:   src/components/Navigation/Index.tsx 	modified:   src/components/Table/Index.tsx 	deleted:    src/containers/Shop/shopRedux/productSlice.ts 	deleted:    src/containers/Shop/shopRedux/productsSaga.ts 	deleted:    src/containers/Shop/shopRedux/types2.ts 	modified:   src/containers/UserTable/Index.tsx 	deleted:    src/containers/UserTable/userRedux/types.ts 	deleted:    src/containers/UserTable/userRedux/userSaga.ts 	deleted:    src/containers/UserTable/userRedux/userslice.ts

--- src/containers/Shop/Index.tsx
+++ src/containers/Shop/Index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Card, Grid, Layout,Row } from "antd";
+import { Card, Grid, Layout, Row } from "antd";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getApiFetch } from "./shopRedux/productSlice";
+import { getApiFetch } from "./shopSlice";
+import { addToCart } from "../Cart/cartslice";
 
 const { Content } = Layout;
-
 const { Meta } = Card;
 
 const ProductData = () => {
@@ -16,33 +16,41 @@ const ProductData = () => {
     dispatch(getApiFetch());
   }, [dispatch]);
 
-  // useEffect(() => {
-  //   console.log(products);
-  // }, [products]);
-
-  console.log(products[10]);
+  const handleAddProduct = (product: any) => {
+    dispatch(addToCart(product));
+  };
 
   return (
     <Content>
-      <Row gutter = {50}>
-      {products.map((p: any) => {
-        return (
-          <>
-          <Content style={{ padding: "50px"}}>
-            
-            <Card
-              hoverable
-              style={{ width: 240}}
-              cover={<img alt="bleh" src={p.image} />}
-            >
-              <Meta title={p.title}/>
-            </Card>
-
-            </Content>
-            ;
-          </>
-        );
-      })}
+      <Row gutter={50}>
+        {products.map((p: any) => {
+          return (
+            <>
+              <Content style={{ padding: "50px" }}>
+                <Card
+                  hoverable
+                  style={{
+                    width: 240,
+                    textAlign: "center",
+                    marginTop: "12px",
+                    fontSize: "15px",
+                    fontWeight: "700",
+                  }}
+                  cover={<img alt="bleh" src={p.image} />}
+                >
+                  <Meta title={p.title} />
+                  <button
+                    style={{ margin: "10px" }}
+                    onClick={() => handleAddProduct(p)}
+                  >
+                    Add to cart
+                  </button>
+                </Card>
+              </Content>
+              ;
+            </>
+          );
+        })}
       </Row>
     </Content>
   );
