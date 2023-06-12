05995244eb53d6b770f633a2b42ff137fbdc3935
YousafAhmed <yousafahmed537@gmail.com>
2023-06-12 15:30:46 +0500

    modified:   src/containers/Homepage/Index.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	modified:   src/components/Form/Index.tsx 	modified:   src/components/Navigation/Index.tsx 	modified:   src/components/Table/Index.tsx 	modified:   src/containers/Cart/cartItem.tsx 	modified:   src/containers/Cart/cartslice.ts 	modified:   src/containers/Main/Index.tsx 	modified:   src/containers/Shop/Index.tsx 	src/containers/Cart/styles.tsx 	src/containers/Homepage/styles.tsx 	src/containers/Shop/styles.tsx

--- src/containers/Homepage/Index.tsx
+++ src/containers/Homepage/Index.tsx
@@ -1,37 +1,46 @@
-import { Space, Carousel } from "antd";
+import { Carousel, Typography } from "antd";
 import React from "react";
-import Cart from "../Cart";
-import { title } from "process";
-
+import { Container, Heading, SubContainer } from "./styles";
 
 const contentStyle: React.CSSProperties = {
-  height: '400px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: "lightblue",
-  
+  height: "400px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "linear-gradient(to right, #bdc3c7, #2c3e50)",
 };
-
 
 const Page = () => {
   return (
-    <div>
-     <Carousel autoplay style={{marginTop:"60px"}}>
-    <div>
-      <h3 style={contentStyle}>{title}</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel>
-    </div>
+    <Container>
+      <Carousel>
+        <SubContainer>
+          <img
+            src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+            style={{
+              borderColor: "black",
+              display: "flex",
+              width: "20%",
+              margin: "auto",
+              float: "left",
+              marginLeft: "80px",
+              marginTop: "10px",
+              borderRadius: "35%",
+            }}
+          />
+          <Heading>Get your wares at cheap price with quality</Heading>
+        </SubContainer>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </Container>
   );
 };
 
